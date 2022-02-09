51fea660164f1b7d2ce2a72e3b76e0f4b5b98bbe
sahincan <ozbakirsahincan@gmail.com>
2022-02-09 20:45:29 +0300

    bitti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card';
+  posts = [
+    {
+      title:'Dag Tas',
+      userName:'sahincan',
+      imageUrl:'assets/mountain.jpeg',
+      content : 'Yanlız iyi tırmandık ha ...'
+    },
+    { 
+      title:'Bisikletli',
+      userName:'denizbilenozbakir',
+      imageUrl:'assets/biking.jpeg',
+      content : 'Biking ...'
+    },
+    {
+      title:'Hani Kuşlar Ağaçlar',
+      userName:'cookie',
+      imageUrl:'assets/tree.jpeg',
+      content : 'Hav hav hav ...'
+    },
+    {
+      title:'Agu',
+      userName:'laçin',
+      imageUrl:'assets/mountain.jpeg',
+      content : 'ıngağ  ...'
+    },
+  ]
 }
